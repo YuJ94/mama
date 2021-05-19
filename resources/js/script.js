1f5a2ccb636418f3912fafd90740be2f09459bec
unknown <jyu8369@yahoo.com>
2021-05-19 13:23:32 -0500

    Updated mobile menu and testimonial sections.

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,3 +6,46 @@ const body = document.querySelector("body");
 menuToggler.addEventListener('click', function () {
     body.classList.toggle("open");
 });
+
+// CAROUSEL VARIABLES
+
+const carouselBox = document.querySelector('.carousel-box');
+const carouselText = document.querySelectorAll('.carousel-text');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//COUNTER
+
+let counter = 1;
+const size = carouselText[0].clientWidth;
+
+carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//BUTTON LISTENERS
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselText.length - 1) return;
+    carouselBox.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselBox.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselBox.addEventListener('transitionend', () => {
+    if (carouselText[counter].id === 'lastClone') {
+        carouselBox.style.transition = "none";
+        counter = carouselText.length - 2;
+        carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselText[counter].id === 'firstClone') {
+        carouselBox.style.transition = "none";
+        counter = carouselText.length - counter;
+        carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
