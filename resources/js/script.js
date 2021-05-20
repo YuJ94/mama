e1d59ea19875a450f053a65474ffb29d03f328e3
unknown <jyu8369@yahoo.com>
2021-05-19 21:21:04 -0500

    Added ScrollReveal library.

--- resources/js/script.js
+++ resources/js/script.js
@@ -49,3 +49,34 @@ carouselBox.addEventListener('transitionend', () => {
         carouselBox.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 });
+
+//SCROLL REVEAL
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
