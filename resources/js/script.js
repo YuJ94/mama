db038ce3bcdb613e4ba1742c90c838032b00b7de
unknown <jyu8369@yahoo.com>
2021-05-19 21:53:16 -0500

    Updated animations.

--- resources/js/script.js
+++ resources/js/script.js
@@ -57,26 +57,26 @@ sr.reveal('.animate-left', {
     origin: 'left',
     duration: 1000,
     distance: '25rem',
-    delay: 300
+    delay: 600
 });
 
 sr.reveal('.animate-right', {
     origin: 'right',
     duration: 1000,
     distance: '25rem',
-    delay: 300
+    delay: 600
 });
 
 sr.reveal('.animate-top', {
     origin: 'top',
     duration: 1000,
     distance: '25rem',
-    delay: 300
+    delay: 600
 });
 
 sr.reveal('.animate-bottom', {
     origin: 'bottom',
     duration: 1000,
     distance: '25rem',
-    delay: 300
+    delay: 600
 });
